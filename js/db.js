802f62cd5d1c57d715d0eaa201f30dbfb043f5a7
hyalki-del <hyalki@gmail.com>
2026-08-23 01:44:44 +0300

    Refactor LocalStorageEngine constructor and methods

--- js/db.js
+++ js/db.js
@@ -1,31 +1,28 @@
 class LocalStorageEngine {
-    constructor(dbName = 'ArchitecturalAgendaDB', dbVersion = 1) {
+    constructor(dbName = 'ArchVectorDB', version = 1) {
         this.dbName = dbName;
-        this.dbVersion = dbVersion;
+        this.version = version;
         this.db = null;
     }
 
     async init() {
         return new Promise((resolve, reject) => {
-            const request = indexedDB.open(this.dbName, this.dbVersion);
-            request.onupgradeneeded = (e) => {
+            const req = indexedDB.open(this.dbName, this.version);
+            req.onupgradeneeded = (e) => {
                 const db = e.target.result;
                 if (!db.objectStoreNames.contains('entries')) {
                     db.createObjectStore('entries', { keyPath: 'entryId' });
                 }
             };
-            request.onsuccess = (e) => {
-                this.db = e.target.result;
-                resolve();
-            };
-            request.onerror = (e) => reject(e.target.error);
+            req.onsuccess = (e) => { this.db = e.target.result; resolve(); };
+            req.onerror = (e) => reject(e.target.error);
         });
     }
 
-    async saveEntry(entry) {
+    async saveEntry(record) {
         return new Promise((resolve, reject) => {
             const tx = this.db.transaction('entries', 'readwrite');
-            tx.objectStore('entries').put(entry);
+            tx.objectStore('entries').put(record);
             tx.oncomplete = () => resolve();
             tx.onerror = (e) => reject(e.target.error);
         });
@@ -34,9 +31,9 @@ class LocalStorageEngine {
     async getEntry(entryId) {
         return new Promise((resolve, reject) => {
             const tx = this.db.transaction('entries', 'readonly');
-            const request = tx.objectStore('entries').get(entryId);
-            request.onsuccess = () => resolve(request.result);
-            request.onerror = (e) => reject(e.target.error);
+            const req = tx.objectStore('entries').get(entryId);
+            req.onsuccess = () => resolve(req.result);
+            req.onerror = (e) => reject(e.target.error);
         });
     }
 }
