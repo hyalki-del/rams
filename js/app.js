852017ebd31ce0543f1bcfa5b22b513d4e1202d2
hyalki-del <hyalki@gmail.com>
2026-08-23 01:13:21 +0300

    Add configuration constants for app settings

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    GAS_ENDPOINT: 'YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL',
+    DB_NAME: 'ArchitecturalAgendaDB',
+    DB_VERSION: 1,
+    STROKE_COLORS: {
+        vector: '#1A1A1A',
+        annotation: '#FF4800'
+    }
+};
