b260622496b3ddd6c2c8f2f2680f9d0cc5fa701b
hyalki-del <hyalki@gmail.com>
2026-08-25 10:46:34 +0300

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,39 +1,24 @@
 /**
- * Shared Reactive State Store & Pub/Sub Event Hub
+ * CENTRALIZED REACTIVE STATE STORE
  */
-class StateStore {
-    constructor() {
-        const today = new Date().toISOString().split('T')[0];
-        this.state = {
-            currentDate: today,
-            activeEntry: null, // Holds full day record
-            activeTopicId: null,
-            activeLayer: 'L2_Pencil', // L2_Pencil | L3_Redline
-            activeTool: 'pencil', // pencil | eraser
-            isDirty: false
-        };
-        this.listeners = [];
-    }
+export const AppState = {
+    currentDate: new Date().toISOString().split('T')[0],
+    activeSlot: null,
+    activeTopicId: null,
+    activeTopicTitle: '',
+    
+    // Schema: { "08:00": [ { id, title, strokes: [], bgImage: null } ] }
+    agenda: {},
 
-    getState() {
-        return this.state;
-    }
-
-    setState(delta) {
-        this.state = { ...this.state, ...delta };
-        this.notify();
-    }
-
-    subscribe(listener) {
-        this.listeners.push(listener);
-        return () => {
-            this.listeners = this.listeners.filter(l => l !== listener);
-        };
-    }
-
-    notify() {
-        this.listeners.forEach(listener => listener(this.state));
-    }
-}
-
-export const store = new StateStore();
+    // Drafting Canvas Controls
+    currentTool: 'pen', // 'pen', 'eraser'
+    penColor: '#1C1C1E',
+    penSize: 3,
+    activeLayer: 'L2', // 'L1', 'L2', 'L3'
+    
+    // Command Stack for Undo/Redo operations
+    history: [],
+    historyStep: -1,
+    strokes: [],
+    bgImageBase64: null
+};
