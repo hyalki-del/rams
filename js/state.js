1f3be9381b4fca5dd63f9697118965e04f5e0a20
hyalki-del <hyalki@gmail.com>
2026-08-25 09:50:33 +0300

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,13 +1,39 @@
-export const AppState = {
-  currentDate: new Date().toISOString().split('T')[0],
-  config: null,
-  db: null,
-  agenda: {}
-};
+/**
+ * Shared Reactive State Store & Pub/Sub Event Hub
+ */
+class StateStore {
+    constructor() {
+        const today = new Date().toISOString().split('T')[0];
+        this.state = {
+            currentDate: today,
+            activeEntry: null, // Holds full day record
+            activeTopicId: null,
+            activeLayer: 'L2_Pencil', // L2_Pencil | L3_Redline
+            activeTool: 'pencil', // pencil | eraser
+            isDirty: false
+        };
+        this.listeners = [];
+    }
 
-export function getCurrentAgenda() {
-  if (!AppState.agenda[AppState.currentDate]) {
-    AppState.agenda[AppState.currentDate] = [];
-  }
-  return AppState.agenda[AppState.currentDate];
+    getState() {
+        return this.state;
+    }
+
+    setState(delta) {
+        this.state = { ...this.state, ...delta };
+        this.notify();
+    }
+
+    subscribe(listener) {
+        this.listeners.push(listener);
+        return () => {
+            this.listeners = this.listeners.filter(l => l !== listener);
+        };
+    }
+
+    notify() {
+        this.listeners.forEach(listener => listener(this.state));
+    }
 }
+
+export const store = new StateStore();
